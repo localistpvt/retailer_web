344f0fb6e52d113dfce40dda76b789aceb517d8d
sagarawasthi123 <sagarawasthi751@gmail.com>
2026-01-10 14:04:23 +0530

    localist-frontend

---
+++ localist-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-300">
+      <h1 className="text-4xl font-bold text-white">
+         React + Tailwind CSS Starter Template
+      </h1>
+    </div>
+  );
+}
+
+export default App;
